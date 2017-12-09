070388528e2bedcd61b4dacda93832628e276ace
Haleh-Dolati <haleh.dolati@gmail.com>
2017-12-09 14:06:17 -0800

    Visualization

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -174,14 +174,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, display= True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(True)
 
 
 if __name__ == '__main__':
